493f5a8ee1ae317688d5745613b01850a21de877
Kushagra Jindal <kushagra@Kushagras-MacBook-Pro-2.local>
2023-10-24 19:48:11 +0530

    Hello World Contract

---
+++ HelloWorld/wrappers/HelloWorld.ts
@@ -0,0 +1,29 @@
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
+
+export type HelloWorldConfig = {};
+
+export function helloWorldConfigToCell(config: HelloWorldConfig): Cell {
+    return beginCell().endCell();
+}
+
+export class HelloWorld implements Contract {
+    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+
+    static createFromAddress(address: Address) {
+        return new HelloWorld(address);
+    }
+
+    static createFromConfig(config: HelloWorldConfig, code: Cell, workchain = 0) {
+        const data = helloWorldConfigToCell(config);
+        const init = { code, data };
+        return new HelloWorld(contractAddress(workchain, init), init);
+    }
+
+    async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().endCell(),
+        });
+    }
+}
